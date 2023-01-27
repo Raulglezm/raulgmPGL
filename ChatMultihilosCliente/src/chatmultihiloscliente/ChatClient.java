94d9234e1b517e96cec7590ab645c37ac149b5fd
raul <raulglezmartin@gmail.com>
2023-01-27 20:07:26 +0000

    yo que se

--- ChatMultihilosCliente/src/chatmultihiloscliente/ChatClient.java
+++ ChatMultihilosCliente/src/chatmultihiloscliente/ChatClient.java
@@ -7,7 +7,7 @@ public class ChatClient {
     
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("localhost", 6667);
+            Socket socket = new Socket("172.26.12.0", 6667);
             Cliente cliente = new Cliente(socket);
         }catch(IOException ex){
         }
--- ChatMultihilosCliente/src/chatmultihiloscliente/Cliente.java
+++ ChatMultihilosCliente/src/chatmultihiloscliente/Cliente.java
@@ -29,4 +29,6 @@ public class Cliente {
         this.lector.start();
         
     }
+    
+    
 }
